b4f71ab5aea053ecdc4e8bf07acd56d5dbd9102a
YOvJIK <mrnetilo14@gmail.com>
2023-08-05 15:28:40 +0300

    Create ua-UA.js

---
+++ src/translations/ua-UA.js
@@ -0,0 +1,99 @@
+export const translations = {
+  "openDoor.headline": "Відчинено двері",
+  "openDoor.rules": `Ваш хід починається.
+  
+  Будь ласка, витягніть **карту підземелля** і покладіть її лицьовою стороною догори на стіл.
+  
+  Що ви витягли?",
+  "openDoor.button.monster": "карта монстра",
+  "openDoor.button.curse": "прокляття або пастка",
+  "openDoor.button.other": "все інше",
+
+  "applyCurseOrTrap.headline": "застосувати прокляття / пастку",
+  "applyCurseOrTrap.rules": `Прокляття або пастка впливає на вас негайно.
+  
+  ...хіба ви не *щасливчик*?
+  "applyCurseOrTrap.button": "застосовано прокляття",
+
+  "keepOrPlayCard.headline": "Залишити карту чи зіграти?",
+  "keepOrPlayCard.rules": `Ви можете або зіграти карту відразу, або взяти її в руку`,
+  "keepOrPlayCard.button.keep": "Залишити карту",
+  "keepOrPlayCard.button.play": "Зіграти карту",
+
+  "readyForTrouble.headline": "Бути готовим до неприємностей?",
+  "readyForTrouble.rules" `Якщо у вас на руці є карта **монстра**, ви можете битися з ним.
+  
+  Крім того, ви можете просто розграбувати кімнату. *Але тоді вам більше не можна битися!
+  "readyForTrouble.button.fight": "Битися з монстрами",
+  "readyForTrouble.button.takeLoot": "Пограбувати кімнату",
+
+  "takeDungeonCard.headline": "Взяти карту підземелля",
+  "takeDungeonCard.rules" `Витягніть **карту підземелля** лицьовою стороною донизу. Візьміть її в руку або зіграйте карту відразу.`,
+  "takeDungeonCard.button": "карту взято",
+
+  "mildGift.headline": "Легкий подарунок",
+  "mildGift.rules": "У вас на руках більше ніж **5 карт**?",
+  "mildGift.button.moreThanFive": "У мене 6 або більше карт",
+  "mildGift.button.lessOrEqualFive": "У мене <= 5 карт",
+
+  "hasLowestLevel.headline": "Ви найслабший?",
+  "hasLowestLevel.rules":
+    "Чи **тільки ви** маєте **найнижчий рівень гравця** і ніхто інший?",
+  "hasLowestLevel.button.yes": "Так, я - курча",
+  "hasLowestLevel.button.no": "Ні",
+
+  "discard.headline": "Скинути карти",
+  "discard.rules":
+    "Покладіть зайві карти (*кількість ваших карт - 5*) у відбій обличчям донизу.",
+  "discard.button": "OK",
+
+  "charity.headline": "Благодійність!",
+  "charity.rules": "Віддайте зайві карти (*кількість ваших карт - 5*) гравцеві з найнижчим **рівнем гравця**.
+  
+  Якщо кілька гравців мають найнижчий рівень, **розділіть карти**.`,
+  "charity.button": "Карти було поділено",
+
+  "fightOrRun.headline": "Бийся!",
+  "fightOrRun.rules": "Порівняйте суму рівнів усіх монстрів із сумою вашого рівня та бонусів (і, можливо, вашого помічника).
+  
+  Ви можете битися з монстром, якщо рівень вашого героя **більший** за рівень монстра.
+  Можливо, інший гравець хоче допомогти вам? Тільки **один гравець може допомогти вам**.
+  Домовтеся з ним про ціну його допомоги.
+  
+  Якщо ваш рівень нижчий або однаковий з рівнем монстра, єдиний вихід - втекти,
+  "fightOrRun.button.fight": "Битися з монстром",
+  "fightOrRun.button.run": "Спробувати втекти",
+
+  "wait.headline": "Чекати",
+  "wait.rules":
+    "...дайте іншим гравцям 2.6 секунди, щоб приєднатися до бою",
+
+  "recheckConditions.headline": "Щось змінилося",
+  "recheckConditions.rules", "Чи втрутився хтось у бій, змінюючи рівень монстра або ваш рівень?",
+  "recheckConditions.button.somethingChanged": `Так`,
+  "recheckConditions.button.nothingChanged": `Ні`,
+
+  "victory.headline": "Перемога",
+  "victory.rules": `Ви перемогли монстра. **Підвищіть свій рівень.
+  
+  Візьміть скарб обличчям донизу або **відкрийте, якщо у вас був помічник**.
+  
+  Розділіть скарб, якщо про це було домовлено заздалегідь,
+  "victory.button": "Візьміть скарб і підвищіть рівень",
+
+  "dice.headline": "Втеча",
+  "dice.rules": `**кидок!**.
+
+  Вам потрібно **5** або **6** для успішної втечі.`,
+  "dice.button.lowerThanFive": "Результат випадіння кубиків < 5",
+  "dice.button.fiveOrSix": "результат кубика 5 або 6",
+
+  "badThings.headline": "Погані речі",
+  "badThings.rules":
+    "Погані речі трапляються. Можливо, навіть кілька поганих речей...",
+  "badThings.button": "Погані речі трапляються",
+
+  "done.headline": "Поїзд до кінцевої",
+  "done.rules": "Хід наступного гравця",
+  "done.button": "Готово"
+};
